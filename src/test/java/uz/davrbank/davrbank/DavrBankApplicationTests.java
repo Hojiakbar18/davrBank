e3453a770761c6acd73d87a2e52049dd901ae05f
malga <hojiakbar20300@gmail.com>
2022-02-24 09:56:35 +0500

    24.02.2022 : 09:55

---
+++ src/test/java/uz/davrbank/davrbank/DavrBankApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.davrbank.davrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavrBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
